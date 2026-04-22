11d4089028ca9b2ead5e48e7895a25defad96999
Beginner00082 <aliasharafmohamedbabba@gmail.com>
2026-04-22 14:12:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,58 +1,65 @@
-gsap.from(".container", { duration: 2, opacity: 0, stagger: 0.5, y: 500 }); 
+gsap.from(".container", { duration: 2, opacity: 0, stagger: 0.5, y: 500 });
 
-let btns = document.querySelectorAll(".num-button"); 
-let allBtns = document.querySelectorAll(".button"); 
-let resultBox = document.querySelector("#result-box"); 
-let clearBtn = document.querySelector("#clear"); 
+let btns = document.querySelectorAll(".num-button");
+let allBtns = document.querySelectorAll(".button");
+let resultBox = document.querySelector("#result-box");
+let clearBtn = document.querySelector("#clear");
 
-let total = document.querySelector("#total"); 
+let total = document.querySelector("#total");
 
-let btnSpread = [...btns]; 
-let allBtnSpread = [...allBtns]; 
+let btnSpread = [...btns];
+let allBtnSpread = [...allBtns];
 
-// For Number Inputs 
-btnSpread.forEach((button, i) => { 
-    button.addEventListener("click", () => { 
-        // Inner Values for calculator 
+// For Number Inputs
+btnSpread.forEach((button, i) => {
+    button.addEventListener("click", () => {
+        // Inner Values for calculator
 
-        if (resultBox.innerHTML == "0") { 
-            resultBox.innerHTML = ""; 
-        } 
+        if (resultBox.innerHTML == "0") {
+            resultBox.innerHTML = "";
+        }
 
-        let value = btns[i].innerHTML; 
-        resultBox.innerHTML += value; 
-    }); 
-}); 
+        let value = btns[i].innerHTML;
+        resultBox.innerHTML += value;
+    });
+});
 
-// Function to evalute Strings 
-function evaluate(fn) { 
-    return new Function("return " + fn)(); 
-} 
+// Function to evalute Strings
+function evaluate(fn) {
+    return new Function("return " + fn)();
+}
 
-// To calculate All Input 
-total.addEventListener("click", () => { 
-    let allInputs = resultBox.innerHTML; 
+// To calculate All Input
+total.addEventListener("click", () => {
+    let allInputs = resultBox.innerHTML;
 
-    resultBox.innerHTML = evaluate(allInputs); 
+    let result = evaluate(allInputs);
 
-    console.log(evaluate(allInputs)); 
-}); 
+    // EASTER EGG NUMERI FORTUNATI ❤️ by fratellone
+    if (result == 23 || result == 71) {
+        result = "O nummr tuoij";
+    }
 
-// Clear all Inputs 
-clearBtn.addEventListener("click", () => { 
-    resultBox.innerHTML = "0"; 
-}); 
+    resultBox.innerHTML = result;
 
-// Add Css Style Animation 
-allBtnSpread.forEach((button, i) => { 
-    button.addEventListener("click", () => { 
-        let element = allBtns[i]; 
-        element.style.boxShadow = 
-            "inset -6px -6px 16px 0 rgba(255, 255, 255, .5), inset 6px 6px 16px 0 rgba(209, 205, 199, .5)"; 
+    console.log(result);
+});
 
-        setTimeout(function () { 
-            element.style.boxShadow = 
-                " 6px 6px 16px 0 rgba(209, 205, 199, .5),-6px -6px 16px 0 rgba(255, 255, 255, .5)"; 
-        }, 200); 
-    }); 
+// Clear all Inputs
+clearBtn.addEventListener("click", () => {
+    resultBox.innerHTML = "0";
+});
+
+// Add Css Style Animation
+allBtnSpread.forEach((button, i) => {
+    button.addEventListener("click", () => {
+        let element = allBtns[i];
+        element.style.boxShadow =
+            "inset -6px -6px 16px 0 rgba(255, 255, 255,.5), inset 6px 6px 16px 0 rgba(209, 205, 199,.5)";
+
+        setTimeout(function () {
+            element.style.boxShadow =
+                " 6px 6px 16px 0 rgba(209, 205, 199,.5),-6px -6px 16px 0 rgba(255, 255, 255,.5)";
+        }, 200);
+    });
 });
